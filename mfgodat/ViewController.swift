16044d853197059129d6da7413c7175864b9f530
mentor33 <mobark.k11@outlook.com>
2019-10-15 20:36:37 +0200

    Initial Commit

---
+++ mfgodat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mfgodat
+//
+//  Created by Mobark on 15/10/2019.
+//  Copyright © 2019 ff8aaf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
